a6a07edf7569cc5ce3dfea2b678213cd48ab5bc9
oCt-raiN <aathi257800@gmail.com>
2023-12-09 13:00:08 +0530

    done something

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import { Router , ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
-
-
 
 @Component({
   selector: 'app-login',
@@ -15,27 +18,27 @@ export class LoginComponent {
   form: FormGroup;
   loading = false;
   submitted = false;
-  result:any
+  result: any;
   //register
   register: FormGroup;
   reg_submitted = false;
   reg_loading = false;
-  reg_result:any
+  reg_result: any;
   passwordsMatching = false;
   loginError: boolean = false;
-  RegisterError: boolean =false;
+  RegisterError: boolean = false;
+  error_message: any;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router, 
-    private authservice:AuthService,) {}
+    private router: Router,
+    private authservice: AuthService
+  ) {}
 
-    
-    
   ngOnInit() {
-    
     this.form = this.formBuilder.group({
-      email: ['',[Validators.required,Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: [
         '',
         [
@@ -45,89 +48,108 @@ export class LoginComponent {
           ),
           Validators.minLength(8),
         ],
-      ]
+      ],
     });
 
     this.register = this.formBuilder.group({
-      email: ['',[Validators.required,Validators.email]],
-      name: ['',[Validators.required,Validators.pattern(/^[A-z]*$/),Validators.min(3)]],
-      address: ['',[Validators.required,Validators.maxLength(50)]],
-      phonenumber: ['',[Validators.required,Validators.pattern("[0-9]{10}")]],
-      password: ['',[Validators.required,Validators.pattern(
-        /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-      ),Validators.minLength(8)]],
-      confirmpassword: ['',[Validators.required,Validators.pattern(
-        /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-      ),Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.email]],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[A-z]*$/),
+          Validators.min(3),
+        ],
+      ],
+      address: ['', [Validators.required, Validators.maxLength(50)]],
+      phonenumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+          ),
+          Validators.minLength(8),
+        ],
+      ],
+      confirmpassword: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+          ),
+          Validators.minLength(8),
+        ],
+      ],
     });
   }
 
- // convenience getter for easy access to form fields
- get f() { return this.form.controls; }
- get r() { return this.register.controls; }
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.form.controls;
+  }
+  get r() {
+    return this.register.controls;
+  }
 
   onSubmit() {
-   
     this.submitted = true;
     // reset alerts on submit
     // this.alertService.clear();
-     // stop here if form is invalid
-     if (this.form.invalid) {
+    // stop here if form is invalid
+    if (this.form.invalid) {
       return;
-  }
-  this.loading = true;
-  this.authservice.login(this.f['email'].value, this.f['password'].value)
-  .pipe(first())
-  .subscribe({
-    next: (res) => {
-      this.result = res;
-      // window.confirm(this.result.message);
-      // get return url from query parameters or default to home page
-      // const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-      // console.log(this.result)  
-      // this.router.navigateByUrl(returnUrl);
-      this.router.navigate(['/det/profile/view']);
-    },
-    error: (error) => {
-      this.loading = false;
-      this.loginError =true;
-
     }
-    // {
-    //   // this.alertService.error(error);
-    //   // this.loading = false;
-    // }
-    });
-
+    this.loading = true;
+    this.authservice
+      .login(this.f['email'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe({
+        next: (res) => {
+          this.result = res;
+          // window.confirm(this.result.message);
+          // get return url from query parameters or default to home page
+          // const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          // console.log(this.result)
+          // this.router.navigateByUrl(returnUrl);
+          this.router.navigate(['/det/profile/view']);
+        },
+        error: (error) => {
+          this.loading = false;
+          this.loginError = true;
+        },
+        // {
+        //   // this.alertService.error(error);
+        //   // this.loading = false;
+        // }
+      });
   }
 
-  
-
-  
-  Register(){
-
+  Register() {
     this.reg_submitted = true;
 
-    if (this.register.invalid){
+    if (this.register.invalid) {
       return;
     }
     this.reg_loading = true;
-    this.authservice.register(this.register.value)
+    this.authservice
+      .register(this.register.value)
       .pipe(first())
       .subscribe({
-          next: () => {
-              // this.alertService.success('Registration successful', { keepAfterRouteChange: true });
-              this.router.navigate(['../pages-login'], { relativeTo: this.route });
-              window.location.reload();
-          },
-          error: error => {
-            this.RegisterError = true;
-          }
+        next: () => {
+          // this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+          this.router.navigate(['../pages-login'], { relativeTo: this.route });
+          window.location.reload();
+        },
+        error: (error) => {
+          this.error_message = error.error.message;
+          console.log(error);
+          this.RegisterError = true;
+        },
       });
-
-    
   }
-
 
   togglePanel(isSignUp: boolean): void {
     const container = document.getElementById('container');
